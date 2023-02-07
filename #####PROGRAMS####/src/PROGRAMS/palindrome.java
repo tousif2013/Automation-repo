97251e5af2bb0c3a14b4e45e7003a5fb857e057e
Asus i7 <Asus i7@ASUS-TOUSIF>
2023-02-07 15:02:31 +0530

    Both the programs are there

---
+++ #####PROGRAMS####/src/PROGRAMS/palindrome.java
@@ -0,0 +1,29 @@
+package PROGRAMS;
+
+import java.util.Iterator;
+
+import javax.print.attribute.standard.Chromaticity;
+
+public class palindrome {
+public static void main(String[] args) {
+	
+	
+	String m="mom";
+  char[] e = m.toCharArray();
+int	size=m.length();
+	
+  for(int i=size-1;i<=0;i--)
+  {
+	  
+	  
+	  
+	  
+	  System.out.println( i);
+	  
+  }
+	
+	
+	
+	
+}
+}
